bcafe85e01db60a13e1a6f047d02a030b2a9aba8
Manjeet Singh <manjeet6972@gmail.com>
2025-02-13 14:53:18 +0100

    some changes in header file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import '../../pages/style.css';
-import resume from '../../Assets/manjeet-resume.pdf';
+// import resume from '../../Assets/manjeet-resume.pdf';
 
 
 function Header() {
@@ -77,9 +77,9 @@ function Header() {
               Contact
             </Nav.Link>
           </Nav.Item>
-         <a href={resume} download className="btn btn-primary resumebtn">
-          <span className='downloadButton'>Download Resume</span>
-         </a>
+         {/* <a href={resume} download className="btn btn-primary resumebtn"> */}
+          {/* <span className='downloadButton'>Download Resume</span> */}
+         {/* </a> */}
         </Nav>
       </Navbar.Collapse>
 
